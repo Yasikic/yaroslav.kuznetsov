aacbf834e0d2424bd35af2e96bb733c5f72930c8
Richard <25132940+Yasikic@users.noreply.github.com>
2021-06-02 10:13:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+function myFunction() {
 
+   function toggleClass(element, className){
+     for (var i = 0; i < element.length; i++) {
+       element[i].classList.toggle(className);
+     }
+   }
+
+   var page = document.body;
+   page.classList.toggle("dark-mode");
+
+   var section = document.getElementsByTagName("section");
+   var footer = document.getElementsByTagName("footer");
+
+   var iconsFas = document.getElementsByClassName("fas");
+   var iconsFar = document.getElementsByClassName("far");
+   var iconsFab = document.getElementsByClassName("fab");
+   var date = document.getElementsByClassName("date");
+   var actual = document.getElementsByClassName("actual");
+
+   var hover = document.querySelectorAll('.hover');
+   var pgl = document.querySelectorAll('.pgl');
+
+
+   toggleClass(pgl, "dark-pgl");
+   toggleClass(actual, "dark-actual");
+   toggleClass(hover, "dark-hover");
+   toggleClass(section, "dark-mode-section");
+   toggleClass(footer, "dark-mode-footer");
+   toggleClass(iconsFas, "dark-icons");
+   toggleClass(iconsFar, "dark-icons");
+   toggleClass(date, "dark-icons");
+   toggleClass(iconsFab, "dark-icons");
+}
